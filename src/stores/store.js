db02ba66b0df7bd7d618791e96519522f56d89a8
nhtanonymous <57558120+nhtanonymous@users.noreply.github.com>
2020-09-07 22:28:48 +0700

    Update store.js

--- src/stores/store.js
+++ src/stores/store.js
@@ -9,7 +9,7 @@ const store = createStore(
 	inititalState,
 	compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
